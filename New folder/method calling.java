98ac1e22bf477813c39bb59afa8f6b74c788e135
aniket0601 <aniketgethe2017@gmail.com>
2024-08-25 11:59:22 +0530

    new file

---
+++ New folder/method calling.java
@@ -0,0 +1,19 @@
+public class test {
+
+public void m1(){
+
+system.out.println("m1 calling");
+}
+
+public void m2(){
+
+system.out.println("m2 calling");
+}
+
+public static void main(String args[]){
+
+test.m1();
+test.m2();
+
+
+
